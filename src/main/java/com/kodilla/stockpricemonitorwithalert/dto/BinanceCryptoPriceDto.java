bf74d2158f08fbd562d1dc84193304529175eba0
Katarzyna-Kantarowska <kantarowskakasia@gmail.com>
2025-04-16 17:47:19 +0200

    etys and dtos

--- src/main/java/com/kodilla/stockpricemonitorwithalert/dto/BinanceCryptoPriceDto.java
+++ src/main/java/com/kodilla/stockpricemonitorwithalert/dto/BinanceCryptoPriceDto.java
@@ -1,9 +1,6 @@
-package com.kodilla.stockpricemonitorwithalert.dto.binance;
+package com.kodilla.stockpricemonitorwithalert.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.math.BigDecimal;
 
@@ -11,10 +8,11 @@ import java.math.BigDecimal;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class BinanceCryptoPriceDto {
-
     private String symbol;
     private BigDecimal price;
+    private BigDecimal pricePln;
 
 
 }
